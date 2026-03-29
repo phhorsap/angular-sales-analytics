b39d9854927e0b2b7d497557b23d3e78e8790245
phhorsap <43028614+phhorsap@users.noreply.github.com>
2026-03-29 13:19:12 +0000

    feat(core) componets and refine css

--- dashboard-app-horsap/src/app/app.ts
+++ dashboard-app-horsap/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from './nav-bar/nav-bar';
+import { Main } from './main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavBar,Main],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
